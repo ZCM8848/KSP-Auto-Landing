13a62c9b11feab77a38c06d4ea211c0355f1ad38
ZCM <68009258+ZCM8848@users.noreply.github.com>
2024-09-16 12:26:02 +0800

    Changes now reverted
    Python solver!

---
+++ Control/PID.py
@@ -0,0 +1,45 @@
+from .control_utils import lerp, clamp
+
+
+class PID:
+    def __init__(self):
+        self.d_prev = None
+        self.i_prev = None
+        self.p_prev = None
+        self.result = None
+        self.ep = True
+        self.ei = True
+        self.ed = True
+        self.kp = 1
+        self.ki = 0
+        self.kd = 1
+        self.sd = 0
+        self.diff = 0
+        self.integral = 0
+        self.integral_limit = 1
+        self.error_prev = 0
+        self.first = True
+        self.second = True
+        self.dumpf = None
+
+    def update(self, error, dt):
+        if self.first:
+            self.first = False
+            self.error_prev = error
+        elif self.second:
+            self.second = False
+            self.diff = (error - self.error_prev) / dt
+
+        self.integral += error * dt * self.ki
+        self.integral = clamp(self.integral, self.integral_limit, -self.integral_limit)
+        self.diff = lerp(self.diff, (error - self.error_prev) / dt, 1 - self.sd)
+        p = -error * self.kp
+        i = -self.integral
+        d = -self.diff * self.kd
+        self.result = p * (1 if self.ep else 0) + i * (1 if self.ei else 0) + d * (1 if self.ed else 0)
+
+        self.p_prev = p
+        self.i_prev = i
+        self.d_prev = d
+        self.error_prev = error
+        return self.result
---
+++ README.md
@@ -0,0 +1,40 @@
+# KSP-Auto-Landing (beta)
+*Some python scripts enable your Kerbal rocket to land (safely, hopefully) at a fixed point*
+
+---
+
+### CAUTION: 
+This project is still **IN EARLY STAGE OF DEVELOPMENT**, therefore some functions may not yet completed and buggy.
+
+---
+
+### TODO：
+- [x] Fix autopilot problems  
+- [x] Add a boosterback burn and sea landing general sequence  
+- [ ] Improve control algorithm performance (against huge aerodynamic force)  
+      *I'm learning to solve this problem and hope someone can help me*😢
+
+---
+
+### Usage:
+To run the code:
+`python landing.py`
+
+---
+
+### References:
+[G-FOLD-Python](https://github.com/jonnyhyman/G-FOLD-Python)  
+[G-FOLD](https://github.com/Wrg1t/G-FOLD)  
+[GFOLD_KSP](https://github.com/xdedss/GFOLD_KSP)  
+[Lossless Convexification of Nonconvex Control Bound and Pointing Constraints of the Soft Landing Optimal Control Problem](http://www.larsblackmore.com/iee_tcst13.pdf)
+
+---
+
+### Links:
+[GFOLD-solver](https://github.com/ZCM8848/GFOLD-solver)
+
+---
+
+### Demo:
+[Fixed-point landing terminal guidance of rockets based on GFOLD algorithm](https://www.bilibili.com/video/BV1M6gNe7ECU)  
+[Demonstration of the whole process of the return-fixed-point landing algorithm of the New Glenn-like rocket](https://www.bilibili.com/video/BV1oXYDeLE3L/)
